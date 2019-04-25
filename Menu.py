8a237fc9319aac4df5fbb74cb52e3a97bb5fe312
Arsen Aganesov <arsen.aganesov@gmail.com>
2019-04-25 14:04:15 +0300

    Work in progress

--- Menu.py
+++ Menu.py
@@ -1,56 +1,6 @@
-from random import choice
 from Dialer import Dialer
 from Player import Player
 
 
-def counts_points_of_cards(cards):
-    points_add = 0
-    for point in range(2):
-        if len(cards[point]) == 3 \
-                or "К" in cards[point] \
-                or "В" in cards[point] \
-                or "Д" in cards[point]:
-            points_add += 10
-        elif 'Т' in cards[point]:
-            if points_add <= 10:
-                points_add += 11
-            else:
-                points_add += 1
-        else:
-            points_add += int(cards[point][0])
-    return points_add
-
-
 class Main:
-
-    def give_cards_user(self, player):
-        for _ in range(2):
-            player.points.append(choice(self.list_types))
-        print("Ваши карты:\n", player.cards)
-
-    def give_cards_dialer(self, dialer):
-        for _ in range(2):
-            card = choice(self.list_types)
-            dialer.cards.append(card)
-            dialer.points += self.dictionary_cards.get(card)
-        print("Противник получил карты:\n * *")
-        dialer.points += self.dictionary_cards.get(card)
-
-    def take_card(self, exem):
-        card_add = choice(self.list_types)
-        if "Т" in card_add:
-            points = self.dictionary_cards.get(card_add)
-
-    def return_points(self):
-        self.points_player += counts_points_of_cards(self.cards_player)
-        print("Кол-во ваших очков следующее:\n", self.points_player)
-
-    def do_rate(self):
-        self.money_dialer -= 10
-        self.money_player -= 10
-
-
-mn = Main()
-pl = Player()
-dl = Dialer()
-mn.give_cards_user(pl)
+    pass
