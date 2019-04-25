9f0b23a54831fe28ced2a9e40de3e719f60574a0
Arsen Aganesov <arsen.aganesov@gmail.com>
2019-04-25 14:02:13 +0300

    Added method 'do_bet' and 'win'

--- Bank.py
+++ Bank.py
@@ -3,3 +3,14 @@ class Bank:
         self.money_player = 100
         self.money_dialer = 100
 
+    def do_bet(self):
+        self.money_player -= 10
+        self.money_dialer -= 10
+
+    def win(self, winner):
+        if winner == "player":
+            self.money_player += 10
+        elif winner == "dialer":
+            self.money_dialer += 10
+        else:
+            pass
